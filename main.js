87e171ed5c33e2e838b7f0f473b86766fb19f19a
Agu Orcha <aguorcha@gmail.com>
2023-06-14 15:42:17 +0200

    Archivo JSON enlazado y gráficos diarios anclados abajo para que crezcan hacia arriba

---
+++ main.js
@@ -0,0 +1,8 @@
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error al cargar el archivo JSON', error);
+  })
